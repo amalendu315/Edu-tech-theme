cccdeee2a11c3804160f4b000a0c5315f9e20114
amalendu315 <amalendupandey56@gmail.com>
2021-05-27 14:34:21 +0530

    commit1

--- fontangular.php
+++ fontangular.php
@@ -4,7 +4,7 @@ $visitor_email=$_POST['email'];
 $subject=$_POST['subject'];
 $message=$_POST['message'];
 
-$email_from = 'info@website.com';
+$email_from = 'info@amalendu315.github.io';
 $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
               "User Email: $visitor_email.\n".
