0b61d71475216d9efed707fb04e2833a14599c5d
amalendu315 <amalendupandey56@gmail.com>
2021-05-27 14:08:25 +0530

    commit

---
+++ fontangular.php
@@ -0,0 +1,19 @@
+<?php
+$name=$_POST['name'];
+$visitor_email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+
+$email_from = 'info@website.com';
+$email_subject = 'New Form Submission';
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "Subject: $subject.\n".
+              "User Message: $message.\n";
+$to = 'amalendupandey56@gmail.com';
+
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-to: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers)
+?>
